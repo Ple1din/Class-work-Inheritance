4334a2383c9a67d7f3afe661de9b6cf6110da90e
JSP2005 <A01713050@tec.mx>
2025-05-19 19:05:14 -0600

    Add files via upload

---
+++ Shade_Plant.h
@@ -0,0 +1,20 @@
+
+#ifndef SHADE_PLANT
+#define SHADE_PLANT
+
+#include <string>
+#include "Plant.h"
+
+class Shade_Plant: public Plant {
+    private: 
+        int Humidity;
+    public:
+        Shade_Plant();
+        Shade_Plant(int, string, int, Weather);
+        void set_Humidity(int);
+        int get_Humidity();
+        string determineNeeds()override;
+        string determineNeeds(int);
+};
+
+#endif
